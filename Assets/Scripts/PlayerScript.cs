9fb67d974d421bab584be4b9f83b1c2b45ca6e05
fokkerdrei <cnolen@knights.ucf.edu>
2020-10-25 23:37:35 -0400

    Challenge2 commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,19 +5,41 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    private bool facingRight = true;
     private Rigidbody2D rd2d;
 
     public float speed;
 
-    public Text score;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
 
-    private int scoreValue = 0;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
+    private int score;
+    private int lives;
+
+    Animator anim;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        score = 0;
+        SetScoreText ();
+        winText.text = "";
+        lives = 3;
+        SetLivesText ();
+        loseText.text = "";
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
     // Update is called once per frame
@@ -26,16 +48,15 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "Coin")
-        {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
-        }
+        if (facingRight == false && hozMovement > 0)
+   {
+     Flip();
+   }
+else if (facingRight == true && hozMovement < 0)
+   {
+     Flip();
+   }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -48,11 +69,83 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Coin"))
+        {
+            other.gameObject.SetActive (false);
+            score = score + 1;
+            SetScoreText ();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive (false);
+            lives = lives - 1;
+            SetLivesText ();
+        }
+        if (score == 4)
+        {
+            transform.position = new Vector3(40.0f, -0.2f, 0f);
+            lives = 3;
+        }
+    }
+
+    void SetScoreText ()
+    {
+        scoreText.text = "Score: " + score.ToString ();
+        if (score == 8)
+        {
+            winText.text = "You Win! Created by Connor Nolen";
+            musicSource.Stop();
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            musicSource.loop = false;
+        }
+    }
+
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+        if (lives <= 0)
+        {
+            loseText.text = "You Lose!";
+            gameObject.SetActive (false);
+        }
+    }
+
+
     void Update()
     {
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
           Application.Quit();
         }
     }
+    void Flip()
+   {
+     facingRight = !facingRight;
+     Vector2 Scaler = transform.localScale;
+     Scaler.x = Scaler.x * -1;
+     transform.localScale = Scaler;
+   }
 }
